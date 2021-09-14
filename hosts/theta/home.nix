836fecafbeb8d27fa71aa2bdf1ae6dc01e7c4a50
System administrator <me@riscadoa.com>
2021-09-14 14:07:29 +0100

    Configured theta

--- hosts/theta/home.nix
+++ hosts/theta/home.nix
@@ -54,7 +54,23 @@
 
   services.flameshot.enable = true;
 
-  xsession.enable = true;
+  home.file.".xinitrc".text = ''
+    if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+      eval $(dbus-launch --exit-with-session --sh-syntax)
+    fi
+    systemctl --user import-environment DISPLAY XAUTHORITY
+
+    if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+      dbus-update-activation-environment DISPLAY XAUTHORITY
+    fi
+
+    firefox &
+    discord &
+    mattermost-desktop &
+    spotify &
+
+    exec xmonad
+  '';
   xsession.initExtra = ''
     firefox &
     discord &
