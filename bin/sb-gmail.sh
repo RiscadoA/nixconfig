9dcd675c0e570425fd7221b33e43eba26e95acb5
Ricardo Antunes <me@riscadoa.com>
2022-03-01 10:13:55 +0000

    Fix status bar scripts

--- bin/sb-gmail.sh
+++ bin/sb-gmail.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -e '\uf0e0' $(nix-shell -p python --run "python /etc/nixos/scripts/sb-gmail.py")
+echo -e '\uf0e0' $(nix-shell -p python --run "python /etc/nixos/bin/sb-gmail.py")
