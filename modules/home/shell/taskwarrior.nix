9f6f882234cc29f55748436bfefcc42bfd5f9208
Ricardo Antunes <me@riscadoa.com>
2022-08-13 09:20:26 +0100

    Add taskwarrior module

---
+++ modules/home/shell/taskwarrior.nix
@@ -0,0 +1,18 @@
+# modules/home/shell/taskwarrior.nix
+#
+# Author: Ricardo Antunes <me@riscadoa.com>
+# URL:    https://github.com/RiscadoA/nixconfig
+#
+# Taskwarrior home configuration.
+
+{ lib, config, pkgs, ... }:
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.modules.shell.taskwarrior;
+in {
+  options.modules.shell.taskwarrior.enable = mkEnableOption "taskwarrior";
+
+  config = mkIf cfg.enable {
+    programs.taskwarrior.enable = true;
+  };
+}
