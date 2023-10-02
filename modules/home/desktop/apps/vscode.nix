9f1deeaf072deb1b3152424499aa4c623fd29229
Ricardo Antunes <me@riscadoa.com>
2023-10-02 14:00:30 +0100

    refactor: configure vscode properly

--- modules/home/desktop/apps/vscode.nix
+++ modules/home/desktop/apps/vscode.nix
@@ -9,24 +9,52 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.desktop.apps.vscode;
-
-  extensions = (with pkgs.unstable.vscode-extensions; [
-      bbenoist.nix
-      ms-python.python
-      ms-vscode-remote.remote-ssh
-      ms-vscode.cpptools
-      ms-vsliveshare.vsliveshare
-    ]) ++ pkgs.unstable.vscode-utils.extensionsFromVscodeMarketplace [
-  ];
 in
 {
   options.modules.desktop.apps.vscode.enable = mkEnableOption "vscode";
 
   config = mkIf cfg.enable {
-    programs.vscode = {
-      enable = true;
-      package = pkgs.unstable.vscode;
-      extensions = extensions;
-    };
+    home.packages = [
+      (pkgs.unstable.vscode-with-extensions.override {
+        vscodeExtensions = (with pkgs.unstable.vscode-extensions; [
+          mkhl.direnv
+          github.copilot
+          ms-vscode-remote.remote-ssh
+          ms-vsliveshare.vsliveshare
+          pkgs.vscode-extensions.eamodio.gitlens
+
+          # Theme
+          file-icons.file-icons
+
+          # Spell checker
+          streetsidesoftware.code-spell-checker
+
+          # PDF
+          tomoki1207.pdf
+
+          # Markdown
+          yzhang.markdown-all-in-one
+          bierner.markdown-mermaid
+          bierner.markdown-emoji
+
+          # Nix
+          jnoortheen.nix-ide
+          
+          # C++
+          ms-vscode.cpptools
+          ms-vscode.cmake-tools
+
+          # C#
+          ms-dotnettools.csharp
+
+          # Rust
+          rust-lang.rust-analyzer
+          tamasfe.even-better-toml
+
+          # Python
+          ms-python.python
+        ]);
+      })
+    ];
   };
 }
