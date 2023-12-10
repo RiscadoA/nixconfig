5d95ad320ab5638daab15231f05947feecf7d4c7
Ricardo Antunes <me@riscadoa.com>
2023-12-10 13:54:25 +0000

    feat(charon): add hardware.nix

--- hosts/charon/system.nix
+++ hosts/charon/system.nix
@@ -6,7 +6,10 @@
   };
   
   boot.tmp.cleanOnBoot = true;
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   services.openssh.enable = true;
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 }
 
