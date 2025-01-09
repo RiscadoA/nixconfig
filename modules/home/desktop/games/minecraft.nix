b7ff4eb7d4dc6f2149fefdc96f61606e4afbc4f7
Ricardo Antunes <me@riscadoa.com>
2025-01-09 20:54:37 +0100

    feat: use stable prismlauncher

--- modules/home/desktop/games/minecraft.nix
+++ modules/home/desktop/games/minecraft.nix
@@ -14,6 +14,6 @@ in
   options.modules.desktop.games.minecraft.enable = mkEnableOption "minecraft";
 
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.unstable.prismlauncher ];
+    home.packages = [ pkgs.prismlauncher ];
   };
 }
