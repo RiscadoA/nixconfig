8fa8cf58ed554572a6cff006274d9ca9097a7c2d
Ricardo Antunes <me@riscadoa.com>
2025-12-12 10:05:01 +0000

    feat: remove protobuf vscode extension

--- modules/home/desktop/apps/vscode.nix
+++ modules/home/desktop/apps/vscode.nix
@@ -119,9 +119,6 @@ in
           # C#
           ms-dotnettools.csharp
 
-          # Protos
-          zxh404.vscode-proto3
-
           # Rust
           rust-lang.rust-analyzer
           tamasfe.even-better-toml
