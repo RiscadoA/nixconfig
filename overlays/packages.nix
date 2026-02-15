0b96575c8d1322e3168de57ce939eae895fdcc92
Ricardo Antunes <me@riscadoa.com>
2026-02-15 17:17:40 +0000

    feat: update firefly-iii-data-importer to 2.1.0

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -30,4 +30,26 @@
       hash = "sha256-V8JAai4gZ1nzia4kmQVeBwidQ+Sx5A5on3SJGSevrUU=";
     };
   });
+
+  firefly-iii-data-importer = prev.firefly-iii-data-importer.overrideAttrs (oldAttrs: rec {
+    version = "2.1.0";
+    src = oldAttrs.src.override {
+      tag = "v${version}";
+      hash = "sha256-b5aOhTqYZqjdNl3WD69+EwqP/6u0t2FI3OgdZ2x5Suk=";
+    };
+    npmDeps = final.fetchNpmDeps {
+      inherit src;
+      name = "${oldAttrs.pname}-npm-deps";
+      hash = "sha256-5HUCF9LrQFFBLMcDx0h2DtRxO8pGMzR/WK11AeyrWRM=";
+    };
+    vendorHash = "sha256-7dKpBlpAcg3E3QrdEgrc3//73MWjlrw0PyISV/BriYQ=";
+    composerRepository = final.php84.mkComposerRepository {
+      inherit (oldAttrs) pname;
+      inherit src vendorHash version;
+      composerNoDev = true;
+      composerNoPlugins = true;
+      composerNoScripts = true;
+      composerStrictValidation = true;
+    };
+  });
 }
