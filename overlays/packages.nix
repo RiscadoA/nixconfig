6588f6840a50dee5f281b0bd6882d7b8c88b84b4
Ricardo Antunes <me@riscadoa.com>
2026-02-15 17:54:20 +0000

    feat: update firefly-iii

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -31,6 +31,26 @@
     };
   });
 
+  firefly-iii = prev.firefly-iii.overrideAttrs (oldAttrs: rec {
+    version = "6.4.21";
+    src = oldAttrs.src.override {
+      tag = "v${version}";
+      hash = "sha256-A3KhXQP1CIgfBPyyZmOgYFg2NBqjkOUQo3QA7slEH/I=";
+    };
+    npmDeps = final.fetchNpmDeps {
+      inherit src;
+      name = "${oldAttrs.pname}-npm-deps";
+      hash = "sha256-aOJriwmn3wwGTSGOQ5YEJxi4BwviKnyCZR0n3g4sWhU=";
+    };
+    vendorHash = "sha256-Q0xNabSEESfz3gl9np0dNNOInUZFCZH5X9xYb5Z/5pk=";
+    composerVendor = final.php84.mkComposerVendor {
+      inherit (oldAttrs) pname;
+      inherit src vendorHash version;
+      composerStrictValidation = true;
+      strictDeps = true;
+    };
+  });
+
   firefly-iii-data-importer = prev.firefly-iii-data-importer.overrideAttrs (oldAttrs: rec {
     version = "2.1.0";
     src = oldAttrs.src.override {
