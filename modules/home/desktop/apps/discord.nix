f20aa958490a53a6642e84239eb74a8151a0ae4b
Ricardo Antunes <me@riscadoa.com>
2026-07-03 18:24:48 +0100

    revert(discord): remove broken tray icon patch

--- modules/home/desktop/apps/discord.nix
+++ modules/home/desktop/apps/discord.nix
@@ -4,61 +4,16 @@
 # URL:    https://github.com/RiscadoA/nixconfig
 #
 # Discord home configuration.
-#
-# Custom tray icon configuration fetched from https://gist.github.com/InternetUnexplorer/364432d6d54b6362004ddb37c9fe686a.
 
 { lib, config, pkgs, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.desktop.apps.discord;
-  tray-icons = pkgs.runCommand "discord-tray-icons" { } ''
-    ICON_THEME="${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark"
-    ICON_SVG_SIZE=16
-    ICON_PNG_SIZE=128
-    ICONS=("tray"
-           "tray-connected"
-           "tray-deafened"
-           "tray-muted"
-           "tray-speaking"
-           "tray-unread")
-    mkdir -p $out
-    icon_dir="$ICON_THEME/''${ICON_SVG_SIZE}x''${ICON_SVG_SIZE}/panel"
-    for icon in "''${ICONS[@]}"; do
-      ${pkgs.librsvg}/bin/rsvg-convert \
-        -w $ICON_PNG_SIZE              \
-        -h $ICON_PNG_SIZE              \
-        -f png                         \
-        "$icon_dir/discord-$icon.svg"  \
-        > "$out/$icon.png"
-    done
-  '';
-
-  fix-tray-icons = pkgs.writers.writeBash "fix-discord-tray-icons" ''
-    set -euo pipefail
-    shopt -s nullglob
-    config_dir="''${XDG_CONFIG_HOME:-$HOME/.config}"/''${1//-/}
-    [ -d "$config_dir" ] || exit 0
-    for module_dir in "$config_dir"/*/modules/discord_desktop_core; do
-      target="$module_dir/app/images/systemtray/linux"
-      [ -d "$target" ] || continue
-      echo "(fix-tray-icons) Patching tray icons in '$target'..."
-      cp ${tray-icons}/* "$target"
-    done
-  '';
-
-  inject-tray-icon-fix = pkg:
-    pkgs.symlinkJoin {
-      inherit (pkg) name;
-      paths = [ pkg ];
-      postBuild = ''
-        sed -i '2i ${fix-tray-icons} ${pkg.pname}' $out/opt/Discord/Discord
-      '';
-    };
 in
 {
   options.modules.desktop.apps.discord.enable = mkEnableOption "discord";
 
   config = mkIf cfg.enable {
-    home.packages = [ (inject-tray-icon-fix pkgs.unstable.discord) ];
+    home.packages = [ pkgs.unstable.discord ];
   };
 }
